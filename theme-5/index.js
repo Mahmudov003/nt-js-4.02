70ef0c6023c6bca6acde5e0b9b5b705a69087977
Asliddin <asliddinmakhmudov003@mail.com>
2022-12-20 23:52:53 +0500

    end

--- theme-5/index.js
+++ theme-5/index.js
@@ -1,7 +1,11 @@
 // 5. Massivlardan tashkil topgan massiv elementlarini birlashtiruvchi funksiya yozing
 
 function flatten(arr) {
-    return `[${arr.join()}]`;
+    let str = [];
+        arr.forEach(element => {
+            str.push(element.join());
+        });
+        return str;
   }
   const arrays = [["1", "2", "3"], [true], [4, 5, 6]];
   console.log(flatten(arrays)); // ["1", "2", "3", true, 4, 5, 6];
--- theme-6/index.js
+++ theme-6/index.js
@@ -2,19 +2,19 @@
 // 6. 18-25, 26-35, 36-55 yoshdagi ovoz berganlar va ovoz beruvchilar sonini chiqaruvchi funksiya yozing.
 // Chiqishi kerak bo'lgan natija namunasi  pastda ko'rsatilgan
 
-let voterd = [1 ,1
-    //   { name: "Bob", age: 30, voted: true },
-    //   { name: "Jake", age: 32, voted: true },
-    //   { name: "Kate", age: 25, voted: false },
-    //   { name: "Sam", age: 20, voted: false },
-    //   { name: "Phil", age: 21, voted: true },
-    //   { name: "Ed", age: 55, voted: true },
-    //   { name: "Tami", age: 54, voted: true },
-    //   { name: "Mary", age: 31, voted: false },
-    //   { name: "Becky", age: 43, voted: false },
-    //   { name: "Joey", age: 41, voted: true },
-    //   { name: "Jeff", age: 30, voted: true },
-    //   { name: "Zack", age: 19, voted: false },
+let voterd = [
+      { name: "Bob", age: 30, voted: true },
+      { name: "Jake", age: 32, voted: true },
+      { name: "Kate", age: 25, voted: false },
+      { name: "Sam", age: 20, voted: false },
+      { name: "Phil", age: 21, voted: true },
+      { name: "Ed", age: 55, voted: true },
+      { name: "Tami", age: 54, voted: true },
+      { name: "Mary", age: 31, voted: false },
+      { name: "Becky", age: 43, voted: false },
+      { name: "Joey", age: 41, voted: true },
+      { name: "Jeff", age: 30, voted: true },
+      { name: "Zack", age: 19, voted: false },
     ];
     
     function voterResults(arr) {
